cdc23c6448f12284230e35419159e4d0d37c23d8
WanderingPenwing <nicolas.pinson31@gmail.com>
2024-07-29 23:30:41 +0200

    fixed some keybinds

--- config.h
+++ config.h
@@ -46,7 +46,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,					   KEY,	  view,		   {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,		   KEY,	  toggleview,	 {.ui = 1 << TAG} }, \
@@ -58,42 +58,35 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_bg, "-nf", col_fg, "-sb", col_bga, "-sf", col_fga, NULL };
+static const char *dmenucmd[] = { "~/nixos/scripts/dmenu_launcher.sh", NULL };
 static const char *termcmd[]  = { "kodama", NULL };
 
 static const Key keys[] = {
 	/* modifier					 key		function		argument */
 	{ MODKEY,					   XK_d,	  spawn,		  {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,			 XK_Return, spawn,		  {.v = termcmd } },
-	{ MODKEY,					   XK_b,	  togglebar,	  {0} },
 	{ MODKEY,					   XK_j,	  focusstack,	 {.i = +1 } },
 	{ MODKEY,					   XK_k,	  focusstack,	 {.i = -1 } },
 	{ MODKEY,					   XK_i,	  incnmaster,	 {.i = +1 } },
 	{ MODKEY,					   XK_d,	  incnmaster,	 {.i = -1 } },
 	{ MODKEY,					   XK_h,	  setmfact,	   {.f = -0.05} },
 	{ MODKEY,					   XK_l,	  setmfact,	   {.f = +0.05} },
-	{ MODKEY,					   XK_Tab,	view,		   {0} },
-	{ MODKEY|ShiftMask,			 XK_c,	  killclient,	 {0} },
-	{ MODKEY,					   XK_t,	  setlayout,	  {.v = &layouts[0]} },
-	{ MODKEY,					   XK_f,	  setlayout,	  {.v = &layouts[1]} },
-	{ MODKEY,					   XK_m,	  setlayout,	  {.v = &layouts[2]} },
-	{ MODKEY,					   XK_space,  setlayout,	  {0} },
-	{ MODKEY|ShiftMask,			 XK_space,  togglefloating, {0} },
+	{ MODKEY|ShiftMask,			 XK_a,	  killclient,	 {0} },
 	{ MODKEY,					   XK_0,	  view,		   {.ui = ~0 } },
 	{ MODKEY|ShiftMask,			 XK_0,	  tag,			{.ui = ~0 } },
 	{ MODKEY,					   XK_comma,  focusmon,	   {.i = -1 } },
 	{ MODKEY,					   XK_period, focusmon,	   {.i = +1 } },
 	{ MODKEY|ShiftMask,			 XK_comma,  tagmon,		 {.i = -1 } },
 	{ MODKEY|ShiftMask,			 XK_period, tagmon,		 {.i = +1 } },
-	TAGKEYS(						XK_1,					  0)
-	TAGKEYS(						XK_2,					  1)
-	TAGKEYS(						XK_3,					  2)
-	TAGKEYS(						XK_4,					  3)
-	TAGKEYS(						XK_5,					  4)
-	TAGKEYS(						XK_6,					  5)
-	TAGKEYS(						XK_7,					  6)
-	TAGKEYS(						XK_8,					  7)
-	TAGKEYS(						XK_9,					  8)
+	TAGKEYS(						XK_ampersand,			          0)
+	TAGKEYS(						XK_eacute,					      1)
+	TAGKEYS(						XK_quotedbl,					  2)
+	TAGKEYS(						XK_apostrophe,					  3)
+	TAGKEYS(						XK_parenleft,					  4)
+	TAGKEYS(						XK_minus,					      5)
+	TAGKEYS(						XK_egrave,					      6)
+	TAGKEYS(						XK_underscore,					  7)
+	TAGKEYS(						XK_ccedilla,					  8)
 	{ MODKEY|ShiftMask,			 XK_q,	  quit,		   {0} },
 };
 
